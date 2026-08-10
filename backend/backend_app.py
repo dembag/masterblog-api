9c7ac9b68ccb81cf6fb9fe91a5a7441e05bb3409
dembag <jn.denby@gmail.com>
2026-08-10 09:44:51 +0200

    Added functionality for user to add a post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template, abort
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,45 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+@app.route("/api/posts", methods=["GET", "POST"])
 def get_posts():
-    return jsonify(POSTS)
+    """ Sends posts database as JSON
+        and adds new posts to database. """
+    if request.method == "POST":
+        data = request.get_json()
+        title = data.get("title")
+        content = data.get("content")
+
+        # Validate post data
+        missing_fields = []
+        if not title:
+            missing_fields.append("title")
+
+        if not content:
+            missing_fields.append("content")
+
+        if missing_fields:
+            return jsonify({
+                "error": "Missing required fields.",
+                "missing": missing_fields
+            }), 400
+
+        if POSTS:
+            new_post_id = max(POST['id'] for POST in POSTS) + 1
+        else:
+            new_post_id = 1
+
+        new_post = {
+            "id": new_post_id,
+            "title": title,
+            "content": content
+        }
+
+        POSTS.append(new_post)
+
+        return jsonify(POSTS), 201
+    else:
+        return jsonify(POSTS)
 
 
 if __name__ == '__main__':
